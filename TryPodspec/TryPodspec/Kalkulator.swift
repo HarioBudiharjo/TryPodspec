3dcaa5a097b954f17c760ae40e758d5a3516cba0
Hario Budiharjo <hariobudiharjo@gmail.com>
2020-09-03 11:45:36 +0700

    add framework

---
+++ TryPodspec/TryPodspec/Kalkulator.swift
@@ -0,0 +1,9 @@
+//
+//  Kalkulator.swift
+//  TryPodspec
+//
+//  Created by Mobile DevMac on 03/09/20.
+//  Copyright © 2020 Mobile DevMac. All rights reserved.
+//
+
+import Foundation
---
+++ TryPodspec/TryPodspec/TryPodspec.h
@@ -0,0 +1,19 @@
+//
+//  TryPodspec.h
+//  TryPodspec
+//
+//  Created by Mobile DevMac on 03/09/20.
+//  Copyright © 2020 Mobile DevMac. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+//! Project version number for TryPodspec.
+FOUNDATION_EXPORT double TryPodspecVersionNumber;
+
+//! Project version string for TryPodspec.
+FOUNDATION_EXPORT const unsigned char TryPodspecVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <TryPodspec/PublicHeader.h>
+
+
