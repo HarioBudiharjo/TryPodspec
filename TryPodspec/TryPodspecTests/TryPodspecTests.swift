2e46e8dad254ea02fd4bd63476cad867c8af12b5
Hario Budiharjo <hariobudiharjo@gmail.com>
2020-09-03 11:46:02 +0700

    Add unit testing

--- TryPodspec/TryPodspecTests/TryPodspecTests.swift
+++ TryPodspec/TryPodspecTests/TryPodspecTests.swift
@@ -11,17 +11,24 @@ import XCTest
 
 class TryPodspecTests: XCTestCase {
 
-    override func setUpWithError() throws {
-        // Put setup code here. This method is called before the invocation of each test method in the class.
+    func testTambah() throws {
+        XCTAssertEqual(Kalkulator.tambah(1, 2), 3, "Function tambah tidak sesuai!")
+        XCTAssertEqual(Kalkulator.tambah(1.0, 2.5), 3.5, "Function tambah tidak sesuai!")
     }
-
-    override func tearDownWithError() throws {
-        // Put teardown code here. This method is called after the invocation of each test method in the class.
+    
+    func testKurang() throws {
+        XCTAssertEqual(Kalkulator.kurang(1, 2), -1, "Function kurang tidak sesuai!")
+        XCTAssertEqual(Kalkulator.kurang(1.5, 0.5), 1, "Function kurang tidak sesuai!")
     }
-
-    func testExample() throws {
-        // This is an example of a functional test case.
-        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    
+    func testKali() throws {
+        XCTAssertEqual(Kalkulator.kali(1, 2), 2, "Function kali tidak sesuai!")
+        XCTAssertEqual(Kalkulator.kali(1.5, 2), 3, "Function kali tidak sesuai!")
+    }
+    
+    func testBagi() throws {
+        XCTAssertEqual(Kalkulator.bagi(4, 2), 2, "Function bagi tidak sesuai!")
+        XCTAssertEqual(Kalkulator.bagi(5.0, 2), 2.5, "Function bagi tidak sesuai!")
     }
 
     func testPerformanceExample() throws {
